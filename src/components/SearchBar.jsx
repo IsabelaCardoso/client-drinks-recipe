8ad015e1c4fb159aa096ac46c620a29041e5aa5a
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-06-16 17:38:50 -0300

    API fetch for first-letter and drink name

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react';
 import DrinksContext from '../context/Context';
 import useFetch from '../services/useFetch';
+import DrinksCard from './DrinksCard';
 
 function SearchBar() {
   const [inputValues, setInputValues] = useState({});
   const [radioType, setRadioType] = useState('');
   const { searchFetch } = useFetch();
   const { recipes } = useContext(DrinksContext);
+  const [oneWordHidden, setOneWordHidden] = useState(true);
 
   useEffect(() => {
+    // if (inputValues.type === )
     const textingTimer = setTimeout(async() => await searchFetch(inputValues), 3000); // chamada da api
     return () => clearTimeout(textingTimer);
   }, [inputValues])
+
+  const handleChange = (value) => {
+    if (!radioType) return alert('Por favor escolha uma categoria');
+    if (radioType === 'first-letter' && value.length > 1) return setOneWordHidden(false);
+    if (radioType === 'first-letter' && value.length <= 1) return setOneWordHidden(true);
+    // alert('Sua busca deve conter apenas uma letra');
+    setInputValues({search: value, type: radioType})
+    if (!recipes) return 
+  };
 
   return (
     <>
       <form>
         <input
-          onChange={(event) => setInputValues({search: event.target.value, type: radioType})}
+          onChange={(event) => handleChange(event.target.value) }
         />
         <label>
           <input
@@ -38,7 +50,8 @@ function SearchBar() {
           Nome
         </label>
       </form>
-      {/* <p>{ textSearch }</p> */}
+      <span hidden={ oneWordHidden }>Sua busca deve conter apenas uma letra</span>
+      {(recipes && recipes.length > 0) ? <DrinksCard /> : <span>Desculpe, não encontramos nenhuma receita para o filtro selecionado. Tente novamente. </span> }
     </>
   );
 }
